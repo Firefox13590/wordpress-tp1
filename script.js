2f209453eb33ce5ce731c2616a2b5f2df4f06675
Firefox13590 <pharlyb@gmail.com>
2026-03-29 17:23:51 -0400

    freedom

--- script.js
+++ script.js
@@ -21,10 +21,60 @@ console.log(tousLesParagraphes);
 /* 
         FONCTIONS
 */
+/**
+ * Ferme automatiquement le menu burger quand un lien est cliqué (pour les petits écrans)
+ */
 function fermerMenu(){
     const cc = document.getElementById('cc-menu-burger');
     console.log(cc);
     cc.checked =  false;
+}
+
+/**
+ * Change l'image de background de la galerie en fonction du mouvement (mvt) et de l'index de l'image courante (indexCourant).
+ * 
+ * S'asure de garder le nouvel index dans les limites de la liste d'images et met à jour le dataset de la galerie pour le prochain mouvement.
+ * 
+ * @param {Number} mvt La valeur du mouvement (+1 pour la flèche droite, -1 pour la flèche gauche)
+ * @param {Number} indexCourant L'index de l'image courante dans la galerie
+ */
+function gererGalerie(mvt = 0, indexCourant){
+    // console.log(mvt, indexCourant);
+    const galerie = document.querySelector('main section#ici .galerie');
+    // console.log(galerie);
+    const racineImagesGalerie = 'images/espace_pour_la_vie/';
+    const listeImagesGalerie = [
+        'camponotus-23942.jpg',
+        'fourmi_coupeuse_de_feuille_atta_ins0069863_alexandre_robertson_dufour_620x415.jpg',
+        'p_pharaonis.jpg'
+    ];
+    const listeDescriptions = [
+        'Fourmi charpentière',
+        'Fourmi coupeuse de feuille',
+        'Fourmi pharaon'
+    ];
+
+    let nouvelIndex = Number(indexCourant) + mvt;
+    if(nouvelIndex < 0){
+        nouvelIndex = listeImagesGalerie.length - 1;
+    }else if(nouvelIndex > listeImagesGalerie.length - 1){
+        nouvelIndex = 0;
+    }
+    // console.log(nouvelIndex);
+
+    galerie.style.backgroundImage = `url('${racineImagesGalerie + listeImagesGalerie[nouvelIndex]}')`;
+    galerie.dataset.galerieIndex = nouvelIndex;
+    galerie.title = listeDescriptions[nouvelIndex];
+}
+
+/**
+ * Récupère la valeur de mouvement (+/-1) et l'index de l'image de la galerie pour faire le changement d'image
+ * @param {HTMLElement} el L'élément cliqué (flèche de la galerie)
+ * @param {Number} mvt La valeur du mouvement (+1 pour la flèche droite, -1 pour la flèche gauche)
+ */
+function bougerGalerie(el, mvt){
+    // console.log(el, mvt);
+    gererGalerie(mvt, el.parentElement.dataset.galerieIndex);
 }
 
 
@@ -47,4 +97,12 @@ liensNavigation.forEach(lien => lien.addEventListener('click', fermerMenu));
 tousLesParagraphes.forEach(p => {
     if(p !== p.parentElement.lastElementChild) {p.after(document.createElement('br'));}
 });
+// mettre une couleur de bg a chaque section
+document.querySelectorAll('main section').forEach(
+    (el, i) => {
+        // console.log(el);
+        el.style.backgroundColor = getComputedStyle(document.querySelector(':root')).getPropertyValue(`--couleur${i + 1}`);
+        el.style.color = [0, 2].includes(i) ? 'white' : 'black';
+    }
+);
 
